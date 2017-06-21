dc23d32d8fdc53705257568ddc97540f12ebbd59
Esther Suk <sestherr@gmail.com>
2017-06-21 00:23:42 -0700

    added a lot more styling

--- app.js
+++ app.js
@@ -1,70 +1,111 @@
 'use strict';
 
+var randomIndex1;
+var randomIndex2;
+var randomIndex3;
+var surveyLength = 0;
+
 var imgArray = [];
+var imgNameArray = [];
 var pathArray = [];
 var arrayOfThree = [];
+
+var createButtomEl = document.createElement('button');
+var subBanners = document.getElementById('subBanners');
+var img1 = document.getElementById('img1');
+var img2 = document.getElementById('img2');
+var img3 = document.getElementById('img3');
 
 function imgMaker(name,path) {
   this.path = path;
   this.name = name;
-  this. timesAppeared = 0;
-  this. timesClicked = 0;
+  this.timesAppeared = 0;
+  this.timesClicked = 0;
   imgArray.push(this);
-  pathArray.push(this.path);
+  imgNameArray.push(this.name);
 };
 
-new imgMaker('bag', 'img/bag.jpg');
-new imgMaker('banana', 'img/banana.jpg');
-new imgMaker('bathroom', 'img/bathroom.jpg');
-new imgMaker('boots', 'img/boots.jpg');
-new imgMaker('breakfast', 'img/breakfast.jpg');
-new imgMaker('bubblegum', 'img/bubblegum.jpg');
-new imgMaker('bus', 'img/bus.jpg');
-new imgMaker('chair', 'img/chair.jpg');
-new imgMaker('cthulhu', 'img/cthulhu.jpg');
-new imgMaker('dragon', 'img/dragon.jpg');
-new imgMaker('pen', 'img/pen.jpg');
-new imgMaker('pet-sweep', 'img/pet-sweep.jpg');
-new imgMaker('scissors', 'img/scissors.jpg');
-new imgMaker('shark', 'img/shark.jpg');
-new imgMaker('sweep', 'img/sweep.jpg');
-new imgMaker('tauntaun', 'img/tauntaun.jpg');
-new imgMaker('unicorn', 'img/unicorn.jpg');
-new imgMaker('usb', 'img/usb.jpg');
-new imgMaker('water-can', 'img/water-can.jpg');
-new imgMaker('wine-glass', 'img/wine-glass.jpg');
+for( var i = 0; i < imgArray.length; i++) {
+  imageNameArray.push(imageArray[i].name);
+}
+function createCatalog () {
+  new imgMaker('bag', 'img/bag.jpg');
+  new imgMaker('banana', 'img/banana.jpg');
+  new imgMaker('bathroom', 'img/bathroom.jpg');
+  new imgMaker('boots', 'img/boots.jpg');
+  new imgMaker('breakfast', 'img/breakfast.jpg');
+  new imgMaker('bubblegum', 'img/bubblegum.jpg');
+  new imgMaker('bus', 'img/bus.jpg');
+  new imgMaker('chair', 'img/chair.jpg');
+  new imgMaker('cthulhu', 'img/cthulhu.jpg');
+  new imgMaker('dog-duck', 'dog-duck.jpg');
+  new imgMaker('dragon', 'img/dragon.jpg');
+  new imgMaker('pen', 'img/pen.jpg');
+  new imgMaker('pet-sweep', 'img/pet-sweep.jpg');
+  new imgMaker('scissors', 'img/scissors.jpg');
+  new imgMaker('shark', 'img/shark.jpg');
+  new imgMaker('sweep', 'img/sweep.png');
+  new imgMaker('tauntaun', 'img/tauntaun.jpg');
+  new imgMaker('unicorn', 'img/unicorn.jpg');
+  new imgMaker('usb', 'img/usb.gif');
+  new imgMaker('water-can', 'img/water-can.jpg');
+  new imgMaker('wine-glass', 'img/wine-glass.jpg');
+}
+
+function randomIndex() {
+  randomIndex1 = Math.floor(Math.random() * imgArray.length);
+  randomIndex2 = Math.floor(Math.random() * imgArray.length);
+  randomIndex3 = Math.floor(Math.random() * imgArray.length);
+}
+function surveyEnd() {
+  createButtonEl.textContent = 'View results';
+  subBanners.appendChild(createButtonEl);
+  subBanners.removeEventListener('click', handleClick);
+}
 
 function randomPic() {
-  var tempArray = [];
-  for (var i = 0; i < 3; i++) {
-    var randomNum = Math.floor(Math.random() * imgArray.length + 1);
-    tempArray.push(imgArray [randomNum].path);
+  if (surveyLength > 24 ) {
+    surveyEnd();
   }
-  while( tempArray [0] === tempArray [1] || tempArray [1] === tempArray [2] || tempArray [2] === tempArray [3] || tempArray.length < 3) {
-    console.log('dupe spotted');
-  };
+  var lastIndex = [];
+  lastIndex.push(randomIndex1);
+  lastIndex.push(randomIndex2);
+  lastIndex.push(randomIndex3);
+  randomIndex();
+  while (randomIndex1 === lastIndex[0] || randomIndex1 === lastIndex[1] || randomIndex1 === lastIndex[2] || randomIndex2 === lastIndex[0] || randomIndex2 === lastIndex[1] || randomIndex2 === lastIndex[2] || randomIndex3 === lastIndex[0] || randomIndex3 === lastIndex[1] || randomIndex3 === lastIndex[2] || randomIndex1 === randomIndex2 || randomIndex1 === randomIndex3 || randomIndex2 === randomIndex3) {
+    randomIndex();
+  }
+  img1.src = imgArray[randomIndex1].path;
+  img2.src = imgArray[randomIndex2].path;
+  img3.src = imgArray[randomIndex3].path;
+
+  imgArray[randomIndex1].timesAppeared += 1;
+  imgArray[randomIndex2].timesAppeared += 1;
+  imgArray[randomIndex3].timesAppeared += 1;
 }
-document.getElementById('img1').src = tempArray[0];
-document.getElementById('img2').src = tempArray[1];
-document.getElementById('img3').src = tempArray[2];
 
+function handleClick(e) {
+  e.preventDefault();
+
+  if (e.target.id === 'img1') {
+    imgArray[randomIndex1].timesClicked += 1;
+  }
+  else if (e.target.id === 'img2') {
+    imgArray[randomIndex2].timesClicked += 1;
+  }
+  else if (e.target.id === 'img3') {
+    imgArray[randomIndex3].timesClicked += 1;
+  }
+  else {
+    alert('Pick a product!');
+    return;
+  }
+  surveyLength += 1;
+  randomPic();
+}
+
+createCatalog();
+randomIndex();
 randomPic();
-// while(right === center || right === left || center === left) {
-//   console.log( 'dupe spotted' );
-//   randomPic(); //call function to re-roll
-// }
 
-// Roll Pic 1
-// Roll Pic 2
-// while (1 === 2) {
-//   console.log("dupe spotted")
-//   re-roll
-// }
-// Pic 3
-// while ( 1 === 3 ) ( 2 === 3 ) {
-//   re-roll
-// }
-// container.addEventListeners('clicks', handleClick);
-
-// function handleClick(event) {
-//   console.log(event.target.id);
+subBanners.addEventListener('click', handleClick);
